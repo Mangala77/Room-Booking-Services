2c7e6b45a5368a90e2e5103aa650ec4aaa94bca1
Mangala <mangala.melkunde@northgateps.com>
2018-03-13 16:51:41 +0000

    proper indentation

--- spec/services/booking_service_spec.rb
+++ spec/services/booking_service_spec.rb
@@ -18,54 +18,54 @@ describe BookingService do
 
     it 'should not create booking and return false' do
       expect(subject).to eq(false)
-    end      	
+    end	
   end
 
   context 'when start date is less than end date' do
     let(:booking_params) do
-   	  { start: Date.today, end: Date.today - 4, room_id: room.id }
-   	end
-   	
-    subject { described_class.new(booking_params).call }
+      { start: Date.today, end: Date.today - 4, room_id: room.id }
+    end
 
-   	it 'should not create booking and return false' do
-      expect(subject).to eq(false)
-    end      	
-  end
-
-  context 'when user enter start date  is between booked start date and end date' do
-    let(:booking_params) do
-   	  { start: Date.today, end: Date.today + 3, room_id: room.id }
-   	end
-
-    before do
-   	  FactoryGirl.create(:booking, start: Date.today-2, room: room)
-   	end
     subject { described_class.new(booking_params).call }
 
     it 'should not create booking and return false' do
       expect(subject).to eq(false)
-   	end 
+    end
+  end
+
+  context 'when user enter start date  is between booked start date and end date' do
+    let(:booking_params) do
+      { start: Date.today, end: Date.today + 3, room_id: room.id }
+    end
+
+    before do
+      FactoryGirl.create(:booking, start: Date.today - 2, room: room)
+    end
+    subject { described_class.new(booking_params).call }
+
+    it 'should not create booking and return false' do
+      expect(subject).to eq(false)
+    end
   end
 
   context 'when user enter end date  is between booked start date and end date' do
     let(:booking_params) do
-   	  { start: Date.today, end: Date.today + 1, room_id: room.id }
-   	end
+      { start: Date.today, end: Date.today + 1, room_id: room.id }
+    end
 
     before do
-   	  FactoryGirl.create(:booking, end: Date.today+4, room: room)
-   	end
+      FactoryGirl.create(:booking, end: Date.today + 4, room: room)
+    end
     subject { described_class.new(booking_params).call }
 
-   	it 'should not create booking and return false' do
+    it 'should not create booking and return false' do
       expect(subject).to eq(false)
-   	end
+    end
   end
 
   context 'when no booking is done for user enter startdate and enddate' do
     let(:booking_params) do
-   	  { start: Date.today, end: Date.today + 3, room_id: room.id }
+      { start: Date.today, end: Date.today + 3, room_id: room.id }
     end
 
     subject { described_class.new(booking_params).call }
